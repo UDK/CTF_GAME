dc0a480a0546618a12ab844485d3547fda7b5bb0
UDK <udk1@bk.ru>
2020-02-17 20:22:49 +0300

    Создал интерфейс для объектов карты и поправил взаимодействие с клиентом

--- Controllers/Server.cs
+++ Controllers/Server.cs
@@ -13,7 +13,7 @@ namespace CTF_GAME.Controllers
     class Server
     {
         const int _timeOutGetData = 5000;
-        const int _lenghtBuffer = 4096;
+        const int _lenghtBuffer = 1024;
         private int _port = 9090;
         private IPAddress _ipAddress = IPAddress.Parse("127.0.0.1");
         List<CommunicationServer> tcpClients = new List<CommunicationServer>();
@@ -32,13 +32,13 @@ namespace CTF_GAME.Controllers
 
         public Server()
         {
-            StartServer(new TcpListener(IpAddr,Port));
+            StartServer(new TcpListener(IpAddr, Port));
         }
 
         private void StartServer(TcpListener tcpServer)
         {
             tcpServer.Start();
-            while(true)
+            while (true)
             {
                 tcpClients.Add(new CommunicationServer(tcpServer.AcceptTcpClient().GetStream()));
             }
@@ -47,13 +47,12 @@ namespace CTF_GAME.Controllers
         //String надо будет заменить на класс, структуру которая будет приходить(структуру/класс надо ещё сделать)
         static public async Task<string> ReadServerAsync(NetworkStream networkStream)
         {
-            List<byte> resultText =  new List<byte>();
-            byte[] textBuffer = new byte [_lenghtBuffer];
+            List<byte> resultText = new List<byte>();
+            byte[] textBuffer = new byte[_lenghtBuffer];
             int offset = 0;
-                textBuffer = new byte[_lenghtBuffer];
-                await networkStream.ReadAsync(textBuffer, offset, _lenghtBuffer);
-                offset += _lenghtBuffer;
-                resultText.AddRange(textBuffer.Select(element => element));
+            await networkStream.ReadAsync(textBuffer, offset, _lenghtBuffer);
+            offset += _lenghtBuffer;
+            resultText.AddRange(textBuffer.Select(element => element));
             return Encoding.ASCII.GetString(textBuffer);
         }
 
