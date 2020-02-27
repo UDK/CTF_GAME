3fb4213561adf6a174c97ef95a5a4c066514869c
UDK <udk1@bk.ru>
2020-02-27 17:36:00 +0300

    Добавил обработку отключения пользователя(ОСТАЛАСЬ ПРОБЛЕМА С ОЧИЩЕНИЕМ ОПЕРАТИВЫ В GB)

---
+++ Model/ModelForEvent/EventArgsNetworkClose.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CTF_GAME.Model.ModelForEvent
+{
+    public class EventArgsNetworkClose
+    {
+        public EventArgsNetworkClose(int ID) { this.ID = ID; }
+
+        public int ID {get;}
+    }
+}
