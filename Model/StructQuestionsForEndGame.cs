b448f244a3e1f2d44b3ed8c7bb6cf5499bd6e418
UDK <udk1@bk.ru>
2020-03-18 17:59:31 +0300

    Добавил структуру для ответа

---
+++ Model/StructQuestionsForEndGame.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CTF_GAME.Model
+{
+    public class StructQuestionsForEndGame
+    {
+        //Стоит arAnswer и goodAnswer вынести в отдельный класс
+        public StructQuestionsForEndGame(string questions, string [] arAnswer, int goodAnswer)
+        {
+
+        }
+        public string questions { get;private set; }
+
+        public int goodAnswer {private get; set; }
+
+        public string[] arAnswer { get;private set; }
+
+        public bool CheckAnswer(int option)
+        {
+            if(goodAnswer)
+        }
+
+    }
+}
